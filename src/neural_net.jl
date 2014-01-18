daa5310fd0029ca39c1c180f4d3356629098892a
Ben Hamner <ben@benhamner.com>
2014-01-18 14:46:05 -0800

    added normalization

--- src/neural_net.jl
+++ src/neural_net.jl
@@ -1,11 +1,18 @@
 using MachineLearning
 
+function zmuv(x::Array{Float64,2})
+    zm = broadcast(-, x, mean(x,1))
+    res = broadcast(/, zm, std(zm,1))
+    res[isnan(res)]=0.0
+    res
+end
+
 data_path = joinpath(ENV["DataPath"], "Kaggle", "digit-recognizer")
 data, header = readcsv(joinpath(data_path, "train.csv"), Float64, has_header=true)
-x_train = data[:,2:size(data,2)]
+x_train = zmuv(data[:,2:size(data,2)])
 y_train = vec(data[:,1])
 data, header  = readcsv(joinpath(data_path, "test.csv"), Float64, has_header=true)
-x_test = data
+x_test = zmuv(data)
 net = train(x_train, y_train, neural_net_options(stop_criteria=StopAfterIteration(100)))
 predictions = predict(net, x_test)
 f = open(joinpath(data_path, "submission.csv"), "w")
